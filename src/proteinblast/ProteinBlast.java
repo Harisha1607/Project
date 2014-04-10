181f59746c100b49f8b0c64739ba8e322bd0cff6
Harisha1607 <Sandy@10.0.0.9>
2014-04-10 08:38:04 +0530

    I tried step 4....Please have a look....

--- src/proteinblast/ProteinBlast.java
+++ src/proteinblast/ProteinBlast.java
@@ -4,10 +4,12 @@ import java.util.*;
 import org.biojava3.core.sequence.io.*;
 import org.biojava3.core.sequence.DNASequence;
 import org.biojava3.core.sequence.ProteinSequence;
- import java.io.File;
- import java.io.BufferedReader;
- import java.io.FileReader;
- import java.io.IOException;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.FileInputStream;
 public class ProteinBlast {
     public static void main(String[] args) throws IOException, Exception{  
 
@@ -25,10 +27,53 @@ public class ProteinBlast {
                 System.out.println("ERROR!!... Protein Sequences are not equal to 2");
                 System.exit(1);
             }
-            
-                
     }
+    public static int[] costMatrix() throws FileNotFoundException{
+        String[] args = null;
+         String filePath = args[0];
+        File mathFile = new File(filePath);
+
+        // make sure argument exists!
+        if (!mathFile.exists()) {
+            printErrAndQuit("File does not exist!");
+        }
+
+        // opening and reading a Math file
+        FileInputStream inputStream = new FileInputStream(filePath);
+            Scanner scanner = new Scanner(inputStream);
+            // checking for the line format
+            while (scanner.hasNextLine()) {
+                // grab line
+                String line = scanner.nextLine();
+
+                if (line.isEmpty()) {
+                    continue;
+                }
+
+                // Splitting the line into 3 components
+                String[] mathArgs = line.split(":");
+                if (mathArgs.length != 3) {
+                    printErrAndQuit("Invalid line!");
+                }
+            }
+            int costData[] = new int[mathFile.nextInt()];
+            for (int i=0; i<costData.length; i++){
+                costData[i] = mathFile.nextInt();
+                
+            }
+            return costData;
+    }
+    
+    public static void printErrAndQuit(String msg) {
+        System.out.println(msg);
+        System.out.flush();
+        System.exit(-1);
+    }
+    
 }
+                
+    
+
 
  
    
