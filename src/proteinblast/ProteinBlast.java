b3e5379dac1d8830680235139fa789f53f3d017b
Harisha1607 <harisha.kedarasipalli@gmail.com>
2014-04-30 09:08:34 +0530

    Update ProteinBlast.java
    Hey Nick,
    Please have a look of my updated code.
    Thanks!

--- src/proteinblast/ProteinBlast.java
+++ src/proteinblast/ProteinBlast.java
@@ -2,44 +2,65 @@ package proteinblast;
 import java.io.*;
 import java.util.*;
 import org.biojava3.core.sequence.io.*;
-import org.biojava3.core.sequence.DNASequence;
 import org.biojava3.core.sequence.ProteinSequence;
+import org.biojava3.alignment.*;
 import java.io.File;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 import java.io.FileInputStream;
+import org.biojava3.alignment.Alignments.PairwiseSequenceAlignerType;
+import org.biojava3.alignment.template.SequencePair;
+import org.biojava3.core.sequence.compound.AminoAcidCompound;
+import org.biojava3.core.sequence.template.Sequence;
+
 public class ProteinBlast {
+    public static short gop;
+    public static short gep;
+    
     public static void main(String[] args) throws IOException, Exception{  
-
+        
+        //Argument validation
         if(args.length != 4){  
-            System.out.println("One of these are not found: Protein Sequences, Matrix file, GOP, GEP");  
+            System.out.println("Invalid number of arguments");  
+            System.out.println("Usage: Java ProtienBlast Fasta_FILE Matrix_FILE GOP GEP");  
             System.exit(1);  
-        }  
-        LinkedHashMap<String, ProteinSequence> a;
-            a = FastaReaderHelper.readFastaProteinSequence(new File(args[0]));
-            HashSet<String> hs = new HashSet<>(a.keySet());
-            for (ProteinSequence sequence : a.values()) {
-            hs.add(sequence.toString());
-            }
-            if (hs.size()!= 2){
-                System.out.println("ERROR!!... Protein Sequences are not equal to 2");
-                System.exit(1);
-            }
-    }
-    public static int[] costMatrix(int cData[]) throws FileNotFoundException{
-        String[] args = null;
-         String filePath = args[0];
-        File mathFile = new File(filePath);
-
-        // make sure argument exists!
-        if (!mathFile.exists()) {
-            printErrAndQuit("File does not exist!");
         }
-
-        // opening and reading a Math file
-        FileInputStream inputStream = new FileInputStream(filePath);
+        
+        String Fasta_File = args[0];
+        String Matrix_File = args[1];
+        gop = Short.parseShort(args[2]);
+        gep = Short.parseShort(args[3]);
+        
+        //Argument printing
+        System.out.println("Fasta_File:"+Fasta_File);
+        System.out.println("Matrix_File:"+Matrix_File);
+        System.out.println("gop:"+gop);
+        System.out.println("gep:"+gep);
+        
+        //Fasta file validation
+        File fastafile = new File(Fasta_File);
+        if(fastafile.exists())
+            System.out.println("Fasta file " +Fasta_File+" exists");
+        else 
+            printErrAndQuit("Fasta file "+Fasta_File+" does not exist!");
+        
+        //Matrix file validation
+        File matfile = new File(Matrix_File);
+        if(matfile.exists())
+            System.out.println("Matrix file " +Matrix_File+" exists");
+        else 
+            printErrAndQuit("Matrix file " +Matrix_File+" does not exist!");
+        
+        //Reading Fasta file containing protein sequences
+        LinkedHashMap<String, ProteinSequence> a;
+            a = FastaReaderHelper.readFastaProteinSequence(new File(Fasta_File));
+            if (a.size()!= 2)
+                printErrAndQuit("Invalid number of protein Sequences "+a.size());
+            else
+                System.out.println("Total "+a.size()+" protein sequences found in Fasta file");
+            
+            //Reading matrix file for validations
+            FileInputStream inputStream = new FileInputStream(Matrix_File);
             Scanner scanner = new Scanner(inputStream);
             // checking for the line format
             while (scanner.hasNextLine()) {
@@ -49,27 +70,36 @@ public class ProteinBlast {
                 if (line.isEmpty()) {
                     continue;
                 }
-
                 // Splitting the line into 3 components
                 String[] mathArgs = line.split(":");
-                if (mathArgs.length != 3) {
-                    printErrAndQuit("Invalid line!");
-                }
+                if (mathArgs.length != 3)
+                    printErrAndQuit("Invalid line!");   
             }
-            int costData[] = new int[mathFile.nextInt()];
-            for (int i=0; i<costData.length; i++){
-                costData[i] = mathFile.nextInt();
-                
-            }
-            return costData;
+           System.out.println("Matrix file " +Matrix_File+" validated");
+            
+            //Cost matrix calculation
+            List keys = new ArrayList(a.keySet());
+            ProteinSequence seq[] = new ProteinSequence[2];
+            for (int i = 0; i<keys.size(); ++i)
+                seq[i] = (ProteinSequence) keys.get(i);
+            SimpleSubstitutionMatrix costMatrix = costMatrix(seq[0], seq[1], gop, gep, Matrix_File);
     }
     
     public static void printErrAndQuit(String msg) {
         System.out.println(msg);
         System.out.flush();
         System.exit(-1);
-    }
+    } 
     
+    public static SimpleSubstitutionMatrix costMatrix(Sequence seq1, Sequence seq2, Short gop, Short gep, String Matrix_File) throws FileNotFoundException{
+      
+       SimpleSubstitutionMatrix<AminoAcidCompound> matrix = new SimpleSubstitutionMatrix<>();
+       SequencePair<ProteinSequence, AminoAcidCompound> pair = Alignments.getPairwiseAlignment(seq1, seq2,
+               PairwiseSequenceAlignerType.GLOBAL, new SimpleGapPenalty(), matrix);
+       System.out.printf("%n%s vs %s%n%s", pair.getQuery().getAccession(), pair.getTarget().getAccession(), pair);
+     
+       return matrix;
+    } 
 }
                 
     
